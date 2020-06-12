3a9050fa9494292c5a650fe5cb34c239876d4d6e
jkljajic15 <jkljajic15@raf.rs>
2020-06-12 05:56:55 +0200

    follow-button final

---
+++ resources/views/notification-bell.blade.php
@@ -0,0 +1,25 @@
+<li class="nav-item dropdown">
+    <a id="navbarDropdown" class="nav-link " href="#" role="button" data-toggle="dropdown"
+       aria-haspopup="true" aria-expanded="false" v-pre>
+        <i class="{{Auth::user()->unreadNotifications->isEmpty() ? 'far fa-bell': 'fas fa-bell'}}"></i>
+    </a>
+
+    @if(Auth::user()->unreadNotifications)
+        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            @foreach(Auth::user()->unreadNotifications as $notification)
+                <li class="dropdown-item d-flex justify-content-between">
+                    <span class=" text-muted ">
+                        You have a new follower
+                        <a href="/profile/{{$notification->data['id']}}">{{$notification->data['name']}}</a>
+                    </span>
+
+                    <a class="text-muted ml-4" href="{{route('markAsRead',$notification->id)}}">
+
+                        <i class="fas fa-times"></i>
+                    </a>
+
+                </li>
+            @endforeach
+        </ul>
+    @endif
+</li>
